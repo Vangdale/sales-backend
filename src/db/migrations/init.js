d1e0288c753cb5003f838c194a019368e8ee03f7
Jorge <geo.kinder@gmail.com>
2026-03-11 16:54:01 +0100

    "debug"

--- src/db/migrations/init.js
+++ src/db/migrations/init.js
@@ -1,36 +1,5 @@
 import db from "../index.js";
 
-// db.exec(`
-//     CREATE TABLE IF NOT EXISTS deals (
-//         id TEXT PRIMARY KEY,
-//         deal_id TEXT,
-//         title TEXT NOT NULL,
-
-//         price REAL NOT NULL,
-//         original_price REAL NOT NULL,
-
-//         store_id INTEGER,
-//         store_key TEXT,
-
-//         steamAppID TEXT,
-
-//         redirect_slug TEXT UNIQUE,
-
-//         is_affiliate INTEGER DEFAULT 0,
-//         affiliate_url TEXT,
-
-//         dealRating REAL,
-//         metacriticScore INTEGER,
-
-//         clicks INTEGER DEFAULT 0,
-//         created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-
-//         img_url TEXT,
-
-//         is_active INTEGER DEFAULT 1
-//     );
-// `);
-
 db.exec(`
     CREATE TABLE IF NOT EXISTS games (
         id UUID PRIMARY KEY,
