2219a76892b085af05912fd274f35ca4a7c36698
Jorge <geo.kinder@gmail.com>
2026-03-11 02:36:35 +0100

    "fix: normalizeDeal-slugify"

--- src/utils/normalizeDeal.js
+++ src/utils/normalizeDeal.js
@@ -1,18 +1,53 @@
 import { randomUUID } from 'crypto';
 import slugify from "slugify";
+import { STORE_MAP } from "../config/stores.js";
+import { AFFILIATE_BUILDERS } from "../config/affiliateBuilders.js";
+
+
+// export function normalizeDeal(deal) {
+
+//     // const storeInfo = STORE_MAP[deal.storeID] ?? {
+//     //     key: "unknown",
+//     //     affiliate: false,
+//     // };
+
+
+
+//     // const builder = AFFILIATE_BUILDERS[storeInfo.key];
+//     // const affiliateUrl = storeInfo.affiliate && builder ? builder(deal) : null;
+
+//     return {
+//         id: randomUUID(),
+//         dealId: deal.dealID,
+//         title: deal.title,
+//         price: Number(deal.salePrice),
+//         originalPrice: Number(deal.normalPrice),
+//         storeID: deal.storeID,
+//         storeKey: storeInfo.key,
+//         steamAppID: deal.steamAppID || deal.gameInfo?.steamAppID || null,
+//         redirectSlug: randomUUID(),
+//         affiliateUrl,
+//         isAffiliate: Boolean(affiliateUrl),
+//         createdAt: new Date().toISOString(),
+//         is_active: 1,
+//     };
+// }
 
 export function normalizeDeal(raw) {
 
     const imageUrl = raw.steamAppID
-    ? `https://cdn.cloudflare.steamstatic.com/steam/apps/${raw.steamAppID}/header.jpg`
-    : "/placeholder.jpg";
+        ? `https://cdn.cloudflare.steamstatic.com/steam/apps/${raw.steamAppID}/header.jpg`
+        : "/placeholder.jpg";
 
     const game = {
         title: raw.title,
-        slug: slugify(raw.title),
+        slug: slugify(raw.title, {
+            lower: true,
+            strict: true
+        }),
         steamAppID: raw.steamAppID || null,
         metacriticScore: Number(raw.metacriticScore) || 0,
-        imageUrl: imageUrl, 
+        imageUrl: imageUrl,
         createdAt: new Date().toISOString(),
     };
 
@@ -24,6 +59,6 @@ export function normalizeDeal(raw) {
         is_active: 1,
     };
 
-    return {  game , deal };
+    return { game, deal };
 
 };
